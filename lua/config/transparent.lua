6366af565538a507e6905c2eaeaf4df31f6f1d9d
Daniel PL <dasdasd003200@gmail.com>
2025-06-14 16:26:49 -0400

    added transparent funtion space r p in kitty fixed

--- lua/config/transparent.lua
+++ lua/config/transparent.lua
@@ -1,30 +1,30 @@
+-- config/transparent.lua
 local M = {}
-
-local is_transparent = true
+local is_transparent = false -- Inicia sin transparencia
 
 function M.toggle_transparency()
   if is_transparent then
-    vim.cmd("highlight clear Normal")
-    vim.cmd("highlight clear NormalNC")
-    vim.cmd("highlight clear EndOfBuffer")
-    vim.cmd("highlight clear VertSplit")
-    vim.cmd("highlight clear SignColumn")
+    -- QUITAR transparencia (restaurar fondo)
+    -- Necesitas restaurar los colores del tema
+    vim.cmd("colorscheme tokyonight") -- Recarga el tema completo
   else
+    -- ACTIVAR transparencia
     vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
     vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
     vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
-    vim.cmd("highlight VertSplit guibg=NONE")
-    vim.cmd("highlight SignColumn guibg=NONE")
+    vim.cmd("highlight VertSplit guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight SignColumn guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight LineNr guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight CursorLineNr guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight StatusLine guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight StatusLineNC guibg=NONE ctermbg=NONE")
   end
   is_transparent = not is_transparent
 end
 
--- Inicializa transparente al cargar
-function M.init_transparency()
-  if is_transparent then
-    M.toggle_transparency() -- lo activa
-    is_transparent = false -- invertido porque toggle lo cambia
-  end
+-- No necesitas init_transparency si quieres empezar sin transparencia
+function M.get_status()
+  return is_transparent
 end
 
 return M
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,18 +1,20 @@
+-- plugins/tokyonight.lua
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     require("tokyonight").setup({
-      -- style = "storm",
-      transparent = false,
+      -- style = "night", -- o "storm", "moon", "day"
+      transparent = false, -- Mantén false para poder controlar manualmente
       styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+        sidebars = "normal", -- Cambia a "normal" para mejor control
+        floats = "normal", -- Cambia a "normal" para mejor control
       },
+      on_highlights = function(hl, c)
+        -- Puedes personalizar colores aquí si es necesario
+      end,
     })
-
     vim.cmd([[colorscheme tokyonight]])
-    -- require("config.transparent").init_transparency()
   end,
 }
