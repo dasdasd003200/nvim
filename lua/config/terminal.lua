c49bd76e5ff3982d45b14836774d609a52a479c3
Daniel PL <dasdasd003200@gmail.com>
2025-06-19 01:50:24 -0400

    Terminal config mode copy

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -104,6 +104,35 @@ function M.setup_terminal_keymaps()
   vim.keymap.set("n", "i", "i", { buffer = terminal_buf, desc = "Enter terminal mode" })
   vim.keymap.set("n", "a", "a", { buffer = terminal_buf, desc = "Enter terminal mode" })
 
+  -- MODO VISUAL para seleccionar y copiar texto
+  -- Entrar en modo visual con v
+  vim.keymap.set("n", "v", "v", { buffer = terminal_buf, desc = "Enter visual mode" })
+  vim.keymap.set("n", "V", "V", { buffer = terminal_buf, desc = "Enter visual line mode" })
+
+  -- Selección rápida con Shift + flechas
+  vim.keymap.set("n", "<S-Up>", "V<Up>", { buffer = terminal_buf, desc = "Select line up" })
+  vim.keymap.set("n", "<S-Down>", "V<Down>", { buffer = terminal_buf, desc = "Select line down" })
+  vim.keymap.set("n", "<S-Left>", "v<Left>", { buffer = terminal_buf, desc = "Select char left" })
+  vim.keymap.set("n", "<S-Right>", "v<Right>", { buffer = terminal_buf, desc = "Select char right" })
+
+  -- En modo visual, expandir selección con Shift + flechas
+  vim.keymap.set("v", "<S-Up>", "<Up>", { buffer = terminal_buf, desc = "Extend selection up" })
+  vim.keymap.set("v", "<S-Down>", "<Down>", { buffer = terminal_buf, desc = "Extend selection down" })
+  vim.keymap.set("v", "<S-Left>", "<Left>", { buffer = terminal_buf, desc = "Extend selection left" })
+  vim.keymap.set("v", "<S-Right>", "<Right>", { buffer = terminal_buf, desc = "Extend selection right" })
+
+  -- Copiar con 'y' en modo visual
+  vim.keymap.set("v", "y", '"+y<Esc>', { buffer = terminal_buf, desc = "Copy selection to clipboard" })
+
+  -- Copiar línea completa con 'yy' en modo normal
+  vim.keymap.set("n", "yy", '"+yy', { buffer = terminal_buf, desc = "Copy line to clipboard" })
+
+  -- Seleccionar todo con Ctrl+a
+  vim.keymap.set("n", "<C-a>", "ggVG", { buffer = terminal_buf, desc = "Select all" })
+
+  -- Salir del modo visual con Esc
+  vim.keymap.set("v", "<Esc>", "<Esc>", { buffer = terminal_buf, desc = "Exit visual mode" })
+
   -- Configurar opciones del buffer
   vim.schedule(function()
     if vim.api.nvim_buf_is_valid(terminal_buf) then
